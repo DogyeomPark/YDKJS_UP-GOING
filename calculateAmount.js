9c25d3699f70755eaf7ca6c26fd0138d6ef9828b
qkrehfud@gmail.com <147295498+DogyeomPark@users.noreply.github.com>
2024-11-26 10:57:41 +0900

    Practice

---
+++ calculateAmount.js
@@ -0,0 +1,52 @@
+const SPENDING_THRESHOLD = 500;
+const MOBILE_PRICE = 129.99;
+const ACCESSORY_PRICE = 32.06;
+const TAX_RATE = 0.07;
+
+let bank_balance = 648.97;
+let amount = 0;
+
+function calculateTax(amount){
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount){
+    return "$" + amount.toFixed( 3 );
+}
+
+while (amount < bank_balance) {
+    amount = amount += MOBILE_PRICE;
+
+    if(amount < SPENDING_THRESHOLD){
+        amount = amount + ACCESSORY_PRICE;
+    }
+}
+
+function reduceAmount(amount) {
+    console.log("Auto Reducing amount...");
+
+    while (amount > bank_balance) {
+        if (amount - ACCESSORY_PRICE > bank_balance) {
+            amount -= ACCESSORY_PRICE;
+            console.log("Removed an accessory. New amount : " + formatAmount(amount));
+        } else if (amount - MOBILE_PRICE > bank_balance) {
+            amount -= MOBILE_PRICE;
+            console.log("Removed a mobile. New amount: " + formatAmount(amount));
+        } else {
+            break;
+        }
+    }
+
+    amount = amount + calculateTax(amount);
+
+    console.log(
+        "your purchase: " + formatAmount(amount)
+    );
+
+    if (amount > bank_balance) {
+        console.log("Ummm maybe you should check your bank balance...");
+        amount = reduceAmount(amount);
+    }
+
+    console.log("Final purchase amount: " + formatAmount(amount));
+}
