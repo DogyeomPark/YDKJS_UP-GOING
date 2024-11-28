34992a242235122c13e04aed261fc130c2ebc62c
qkrehfud@gmail.com <147295498+DogyeomPark@users.noreply.github.com>
2024-11-28 12:19:56 +0900

    rename & learn more

---
+++ Chapter 2: Into JavaScript/ch_2_1.js
@@ -0,0 +1,27 @@
+// typeof Operator
+
+// type_operator
+
+// let a;
+// typeof a;
+//
+// a = "hello world";
+// typeof a;
+
+// ------------------------
+
+// Objects
+
+let obj = {
+    a: "Hi there",
+    b: 54,
+    c: true
+};
+
+obj.a;
+obj.b;
+obj.c;
+
+obj["a"];
+obj["b"];
+obj["c"];
---
+++ Chapter 2: Into JavaScript/ch_2_2object.js
@@ -0,0 +1,12 @@
+let arr = [
+    "Hi there",
+    43,
+    true
+];
+
+arr[0];
+arr[1];
+arr[2];
+arr.length;
+
+typeof arr;
